5f07bb0fe520b1dc4d7b15153bb9d4c94f5dcaff
maxx541 <96718336+maxx541@users.noreply.github.com>
2026-04-15 21:15:18 +0800

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,14 @@
 """Core modules for YouTube music song request plugin"""
 
+from .config import PluginConfig
+from .downloader import Downloader
+from .model import Platform, Song
+from .platform import BaseMusicPlayer, YouTubeMusic
+from .renderer import MusicRenderer
+from .sender import MusicSender
+from .playlist import Playlist
+from .utils import parse_user_input
+
 __all__ = [
     "PluginConfig",
     "Song",
@@ -11,5 +20,4 @@ __all__ = [
     "MusicSender",
     "Playlist",
     "parse_user_input",
-    "SendMode",
 ]
