4f352fa37672e253bd83c3be603c6f0df4c2c368
maxx541 <96718336+maxx541@users.noreply.github.com>
2026-04-15 15:41:57 +0800

    Add core module initialization for YouTube music plugin

---
+++ core/init.py
@@ -0,0 +1,15 @@
+"""Core modules for YouTube music song request plugin"""
+
+__all__ = [
+    "PluginConfig",
+    "Song",
+    "Platform",
+    "BaseMusicPlayer",
+    "YouTubeMusic",
+    "Downloader",
+    "MusicRenderer",
+    "MusicSender",
+    "Playlist",
+    "parse_user_input",
+    "SendMode",
+]
